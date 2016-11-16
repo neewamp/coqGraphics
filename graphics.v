1b83bddc09941a6491668470c161e5136f03c2b4
dm526513 <dm526513@ohio.edu>
2016-11-16 15:09:16 -0500

    ayy

--- graphics.v
+++ graphics.v
@@ -7,7 +7,10 @@
 (* Let me know if you have any questions *)
 
 Require Import PArith String FMaps FMapAVL ExtrOcamlString.
+Require Import Pnat.
 
+
+Require Export QArith.
 (* We probably need a way to express background and forground idk maybe something in the pixel type?*)
 
 Inductive color: Type :=
@@ -15,7 +18,6 @@ Inductive color: Type :=
 | Blue : color
 | Green : color
 | White : color.
-
 
 (* The type of a single pixel's location*)
 Module pixel_l:=  PairOrderedType Positive_as_OT Positive_as_OT.
@@ -83,12 +85,71 @@ Class graphics_prims (T :Type) :=
       draw_pixel : T -> point -> color -> T;
   }.
 
-Section interp.
-  Context {T : Type} `{graphics_prims T}.
+Notation "a # b" := (Qmake a b) (at level 55, no associativity) : Q_scope.
 
+
+Definition Qabs (q : Q) : Q := 
+match q with
+| Z0 # d => q
+| (Zpos n) # d => q
+| (Zneg n) # d => (Zpos n) # d
+end.
+
+Require Import Coq.PArith.BinPos.
+
+
+
+Fixpoint pos_to_nat (p : positive) : nat :=
+match p with
+| xH => S O
+| xO p' => plus (pos_to_nat p') (pos_to_nat p')
+| xI p' => plus (plus (pos_to_nat p') (pos_to_nat p')) (S O)
+end.
+
+
+Fixpoint nat_to_pos (n : nat) : positive :=
+match n with
+| O => xH
+| S n' => Pos.succ (nat_to_pos n')
+end.
+
+
+
+Section interp.
+  
+  Notation "a # b" := (Qmake a b) (at level 55, no associativity) : Q_scope.
+  Context {T : Type} `{graphics_prims T}.
+  
+
+
+
+  Fixpoint draw_line_rc (t : T) (p : point) (c : color)  (i : nat) (er der: Q) : T :=
+  match i with
+  | O => draw_pixel t p c
+  | S i' => 
+            match p with
+            | (x,y) => draw_pixel 
+                          (if Qle_bool (Qmake 1 1) (Qplus er der)
+                           then draw_line_rc t (x,y+1) c i' (Qplus der (Qminus er (Qmake 1 1))) der
+                           else if Qle_bool  (Qplus er der) (Qmake (Zneg 1) 1)
+                                then draw_line_rc t (x,y-1) c i' (Qplus der (Qplus er (Qmake 1 1))) der
+                                else draw_line_rc t p c i' (Qplus der er) der)
+                       ((x + (nat_to_pos i)),y) c
+            end  
+  end.
+    
+  Fixpoint draw_vline (t : T) (p : point) (c : color) (h : nat) : T :=
+  match h,p with
+  | O, _ => draw_pixel t p c
+  | S h', (x,y) => draw_pixel (draw_vline t p c h') (x,y+(nat_to_pos h)) c
+  end.
+  
   Definition interp_draw_line (t : T) (c : color)
                                 (p1 p2 : point) : T :=
-    draw_pixel t p1 c. (* need to make this actually draw a line 
+  match p1,p2 with
+  | (x1,y1),(x2,y2) => draw_line_rc t p1 c (pos_to_nat (x2 - x1)) (-1 # 1) ((Zpos (y2 - y1)) # (x2 - x1)) 
+  end.
+    (* draw_pixel t p1 c.  need to make this actually draw a line 
                           seems like a david thing*)
   Fixpoint interp (t : T) (e : g_com) : T :=
     match e with
@@ -211,6 +272,7 @@ Extract Constant ocaml_draw_pixel =>
      in 
      Graphics.set_color c;
      Graphics.draw_pixel """" (int_of_pos p))".
+
 
 Definition t : OGState := interp (init_state tt) prog1.
 
